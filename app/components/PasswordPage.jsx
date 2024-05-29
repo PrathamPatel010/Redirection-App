0e205cf7f60aedad17ec4125bf3fba8f7ec66a8e
Pratham Patel <ppratham812@gmail.com>
2024-05-29 18:04:50 +0530

    added password auth

---
+++ app/components/PasswordPage.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+import LinksPage from "@/app/components/LinksPage";
+
+export const PasswordPage = () => {
+    const [password, setPassword] = useState('');
+    const [ack,setAck] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const actualPassword = process.env.NEXT_PUBLIC_PASSWORD;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ password, actualPassword });
+        console.log(password === actualPassword);
+        if (password === actualPassword) {
+            setIsAuthenticated(true);
+        } else {
+            setAck("It's wrong password you trespasser");
+        }
+    };
+
+    if (isAuthenticated) {
+        return <LinksPage />;
+    }
+
+    return (
+        <section className={'flex justify-center items-center'}>
+            <form onSubmit={handleSubmit} className={'flex flex-col gap-2 justify-center'}>
+                <label>Enter the password you fucking trespasser</label>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={'p-2 rounded-md'}
+                    placeholder={'Password'}
+                />
+                <button className={'p-2 rounded-md bg-gray-500 text-white hover:bg-black hover:text-white'} type={'submit'}>Let me Enter</button>
+                <div>{ack}</div>
+            </form>
+        </section>
+    );
+};
--- app/page.js
+++ app/page.js
@@ -1,30 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import LinksPage from "@/app/components/LinksPage";
+import {PasswordPage} from "@/app/components/PasswordPage";
 
 export default function Home() {
   return (
     <main className="p-5 flex flex-col items-center">
-      <div className="text-3xl">
-        Links will be here
-      </div>
-      <div className="mt-5">
-          <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/f7072887e83c45aeaa4fd52270ceac44'}>Backend Engineering Page</Link>
-      </div>
-      <div className="mt-5">
-          <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/System-Design-Simplified-a35cd6cc48054585b973bdf7e33746fe'}>System Design Simplified</Link>
-      </div>
-      <div className="mt-5">
-          <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/DSA-54023c73b03b49e988eac2f7de95a06c'}>DSA</Link>
-      </div>
-      <div className="mt-5">
-          <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/Attack-Mode-eee0190c36b845bb9f2dcbe631420473'}>Attack Mode</Link>
-      </div>
-      <div className="mt-5">
-          <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/Attack-Mode-System-9d5d6985544b40aab5779c2ae62377be'}>Attack Mode System</Link>
-      </div>
-      <div className="mt-5">
-          <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/Quick-Schedule-fab350dca7bb426886df408f34dab55c'}>Quick Schedule</Link>
-      </div>
+      <PasswordPage/>
     </main>
   );
 }
