f3205a17793a16fbdba51a94934f4e8e90e1539a
Pratham Patel <ppratham812@gmail.com>
2024-05-30 22:06:41 +0530

    removed attack mode links

--- app/components/LinksPage.jsx
+++ app/components/LinksPage.jsx
@@ -17,12 +17,6 @@ export default function LinksPage() {
                 <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/DSA-54023c73b03b49e988eac2f7de95a06c'}>DSA</Link>
             </div>
             <div className="mt-5">
-                <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/Attack-Mode-eee0190c36b845bb9f2dcbe631420473'}>Attack Mode</Link>
-            </div>
-            <div className="mt-5">
-                <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/Attack-Mode-System-9d5d6985544b40aab5779c2ae62377be'}>Attack Mode System</Link>
-            </div>
-            <div className="mt-5">
                 <Link className="bg-blue-200 hover:bg-blue-600 hover:text-white rounded-md px-3 py-1" target="_blank" href={'https://prathampatel.notion.site/Quick-Schedule-fab350dca7bb426886df408f34dab55c'}>Quick Schedule</Link>
             </div>
         </>
